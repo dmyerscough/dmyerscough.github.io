70d92edf9f623470b945c88950c6126d16c9cea6
Damian Myerscough <Damian.Myerscough@gmail.com>
2021-07-11 20:13:13 -0700

    Read coordinates from GitHub

--- week9/mapanimation.js
+++ week9/mapanimation.js
@@ -1,18 +1,13 @@
 // This array contains the coordinates for all bus stops between MIT and Harvard
-const busStops = [
-    [-71.093729, 42.359244],
-    [-71.094915, 42.360175],
-    [-71.0958, 42.360698],
-    [-71.099558, 42.362953],
-    [-71.103476, 42.365248],
-    [-71.106067, 42.366806],
-    [-71.108717, 42.368355],
-    [-71.110799, 42.369192],
-    [-71.113095, 42.370218],
-    [-71.115476, 42.372085],
-    [-71.117585, 42.373016],
-    [-71.118625, 42.374863],
-];
+
+let busStops = [[-71.093729, 42.359244]];
+
+async function grabBusStops() {
+    const response = fetch('https://raw.githubusercontent.com/dmyerscough/dmyerscough.github.io/main/week9/data.json');
+    const responseData = (await response).json();
+
+    return responseData;
+}
 
 // TODO: add your own access token
 mapboxgl.accessToken = '';
@@ -25,15 +20,16 @@ let map = new mapboxgl.Map({
     zoom: 14,
 });
 
+grabBusStops().then(coordinates => {
+    busStops = coordinates["coordinates"];
+})
+
 // TODO: add a marker to the map at the first coordinates in the array busStops. The marker variable should be named "marker"
 let marker = new mapboxgl.Marker().setLngLat(busStops[0]).addTo(map);
 
 // counter here represents the index of the current bus stop
 let counter = 0;
 function move() {
-    // TODO: move the marker on the map every 1000ms. Use the function marker.setLngLat() to update the marker coordinates
-    // Use counter to access bus stops in the array busStops
-    // Make sure you call move() after you increment the counter.
     setTimeout(() => {
         if (counter > busStops.length) {
             return;
