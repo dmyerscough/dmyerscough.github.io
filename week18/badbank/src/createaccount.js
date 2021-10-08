a1ac57566ea5b1a867637abab4c9dd83ba4651d4
Damian Myerscough <Damian.Myerscough@gmail.com>
2021-10-08 00:09:56 -0700

    More changes

--- week18/badbank/src/createaccount.js
+++ week18/badbank/src/createaccount.js
@@ -9,10 +9,6 @@ import { Card } from "./context";
 
 function CreateAccount() {
   const [show, setShow] = React.useState(true);
-  const [status, setStatus] = React.useState("");
-  const [name, setName] = React.useState("");
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
 
   const ctx = React.useContext(UserContext);
 
@@ -25,97 +21,98 @@ function CreateAccount() {
     password: Yup.string().min(8, "Too Short").required("Required"),
   });
 
-  function validate(field, label) {
-    if (!field) {
-      setStatus("Error: " + label);
-      setTimeout(() => setStatus(""), 3000);
-      return false;
-    }
-    return true;
-  }
-
-  function handleCreate() {
-    console.log(name, email, password);
-    if (!validate(name, "name")) return;
-    if (!validate(email, "email")) return;
-    if (!validate(password, "password")) return;
-    ctx.users.push({ name, email, password, balance: 100 });
-    setShow(false);
-  }
-
-  function clearForm() {
-    setName("");
-    setEmail("");
-    setPassword("");
-    setShow(true);
-  }
-
   return (
     <Card
       className="mx-auto"
       header="Create Account"
-      status={status}
       txtcolor="black"
       body={
-        <Formik
-          initialValues={{
-            name: "",
-            email: "",
-            password: "",
-          }}
-          validationSchema={CreateAccountSchema}
-          onSubmit={(values) => {
-            setName(values.name);
-            setEmail(values.email);
-            setPassword(values.password);
-            setShow(true);
-          }}
-        >
-          {({ errors, touched, isValid, dirty }) => (
-            <Form>
-              Name
-              <br />
-              <Field
-                className="form-control"
-                name="name"
-                placeholder="Enter name"
-              />
-              {errors.name && touched.name ? <div>{errors.name}</div> : null}
-              <br />
-              Email address
-              <br />
-              <Field
-                className="form-control"
-                name="email"
-                placeholder="Enter email"
-              />
-              {errors.email && touched.email ? <div>{errors.email}</div> : null}
-              <br />
-              Password
-              <br />
-              <Field
-                className="form-control"
-                name="password"
-                placeholder="Enter password"
-                type="password"
-              />
-              {errors.password && touched.password ? (
-                <div>{errors.password}</div>
-              ) : null}
-              <br />
-              <button
-                type="submit"
-                className="btn btn-outline-primary mr-1"
-                disabled={!(isValid && dirty)}
-              >
-                Create Account
-              </button>
-              <button type="submit" className="btn btn-outline-primary mr-1">
-                Clear
-              </button>
-            </Form>
-          )}
-        </Formik>
+        show ? (
+          <Formik
+            initialValues={{
+              username: "",
+              email: "",
+              password: "",
+            }}
+            validationSchema={CreateAccountSchema}
+            onSubmit={(values, { resetForm }) => {
+              setShow(false);
+
+              ctx.users.push({
+                name: values.username,
+                email: values.email,
+                password: values.password,
+                balance: 100,
+              });
+
+              resetForm();
+              console.log("Saved");
+            }}
+          >
+            {({ errors, touched, isValid, dirty }) => (
+              <Form>
+                Name
+                <br />
+                <Field
+                  className="form-control"
+                  name="username"
+                  placeholder="Enter name"
+                  autoComplete="new-username"
+                />
+                {errors.name && touched.name ? <div>{errors.name}</div> : null}
+                <br />
+                Email address
+                <br />
+                <Field
+                  className="form-control"
+                  name="email"
+                  placeholder="Enter email"
+                  autoComplete="new-username"
+                />
+                {errors.email && touched.email ? (
+                  <div>{errors.email}</div>
+                ) : null}
+                <br />
+                Password
+                <br />
+                <Field
+                  className="form-control"
+                  name="password"
+                  placeholder="Enter password"
+                  type="password"
+                  autoComplete="new-password"
+                />
+                {errors.password && touched.password ? (
+                  <div>{errors.password}</div>
+                ) : null}
+                <br />
+                <button
+                  type="submit"
+                  className="btn btn-outline-primary mr-1"
+                  disabled={!(isValid && dirty)}
+                >
+                  Create Account
+                </button>
+                <button type="reset" className="btn btn-outline-primary mr-1">
+                  Clear
+                </button>
+              </Form>
+            )}
+          </Formik>
+        ) : (
+          <>
+            <h5>Success</h5>
+            <button
+              type="submit"
+              className="btn btn-outline-primary mr-1"
+              onClick={() => {
+                setShow(true);
+              }}
+            >
+              Add another account
+            </button>
+          </>
+        )
       }
     />
   );
--- week18/badbank/src/home.js
+++ week18/badbank/src/home.js
@@ -1,16 +1,17 @@
+import { Card } from "./context";
 
-import { Card } from "./context"
-
-function Home(){
-    return (
-        <Card
-            className="mx-auto"
-            txtcolor="black"
-            header="BadBank Landing Module"
-            title="Welcome to the BadBank"
-            body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
-        />
-    );
+function Home() {
+  return (
+    <Card
+      className="mx-auto"
+      txtcolor="black"
+      header="BadBank Landing Module"
+      title="Welcome to the BadBank"
+      body={
+        <img src="bank.png" className="img-fluid" alt="Welcome to BadBank" />
+      }
+    />
+  );
 }
 
-export { Home }
+export { Home };
